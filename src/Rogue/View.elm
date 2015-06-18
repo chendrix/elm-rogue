2e21f02bff991c16c660504beb85957d5b2c872f
Chris Hendrix and Yulia Tolskaya <pair+chendrix+ytolskaya@pivotal.io>
2015-06-18 15:48:57 -0700

    Change view code to use elements

--- src/Rogue/View.elm
+++ src/Rogue/View.elm
@@ -10,24 +10,68 @@ import Array exposing (..)
 import Rogue.Model exposing (..)
 
 view : Game -> Element
-view g = leftAligned (Text.monospace (Text.fromString (toString g.gameMap)))
+view g = viewGameMap g.gameMap
 
-toString : GameMap -> String
-toString {board,start,currentPlayerLocation} =
+viewGameMap : GameMap -> Element
+viewGameMap {board,start,currentPlayerLocation} =
   let
     rowifier =
       (\row ->
         Array.map
           (\cell ->
-            if  | isAt currentPlayerLocation cell -> "@"
-                | not <| isOpen cell -> "#"
-                | otherwise -> "."
+            if  | isAt currentPlayerLocation cell -> person
+                | not <| isOpen cell -> barrier
+                | otherwise -> open
           )
           row
         |> toList
-        |> join ""
+        |> flow right
       )
   in
     Array.map rowifier board
       |> toList
-      |> join "\n"
+      |> flow down
+
+
+txt str = 
+  Text.fromString str
+  |> Text.monospace
+  |> centered
+
+person =
+  txt "@"
+  |> standardize
+
+barrier =
+  square 16
+  |> filled red
+  |> (\sq -> collage 16 16 [sq])
+  |> standardize
+
+open =
+  txt "."
+  |> standardize
+
+standardize : Element -> Element
+standardize el =
+  el
+  |> container 16 16 middle
+
+--toString  =
+--  let
+--    rowifier =
+--      (\row ->
+--        Array.map
+--          (\cell ->
+--            if  | isAt currentPlayerLocation cell -> "@"
+--                | not <| isOpen cell -> "#"
+--                | otherwise -> "."
+--          )
+--          row
+--        |> toList
+--        |> join ""
+--      )
+--  in
+--    Array.map rowifier board
+--      |> toList
+--      |> join "\n"
